c0c41d0e761eab1af0440e371800602a15fe37b6
REBAII1 <Ons.REBAII@esprit.tn>
2025-02-21 05:16:34 +0100

    Ajout du type pour le formulaire de registration

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -11,6 +11,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class RegistrationFormType extends AbstractType
 {
@@ -43,9 +44,18 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Travler' => 'ROLE_TRAVLER',
+                    'Host' => 'ROLE_HOST',
+                ],
+                'multiple' => true,
+                'expanded' => true,
+                'attr' => ['class' => 'role-checkbox'],
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
-                'attr' => ['class' => 'form-check-input'],
+                'attr' => ['class' => 'role-checkbox'],
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
